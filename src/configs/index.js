108bb5661debfbe5dbc3596862337b0ee7d157fc
supacheep <supacheep.1972@gmail.com>
2021-10-19 12:11:12 +0700

    update api url

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_PORT: process.env.NODE_PORT,
   APP_ENV: process.env.APP_ENV,
   NODE_ENV: process.env.NODE_ENV,
-  API: 'http://13.212.219.212:9090/api/v1',
+  API: 'https://thprsmeeting.com/api/v1',
 }
