0b3086d07d1938ae1e3358b463d4f595efbfb72c
supacheep <supacheep.1972@gmail.com>
2021-11-13 13:05:19 +0700

    update api

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_PORT: process.env.NODE_PORT,
   APP_ENV: process.env.APP_ENV,
   NODE_ENV: process.env.NODE_ENV,
-  API: 'http://54.179.143.118:9090/api/v1',
+  API: 'https://api.icsmeeting.com/api/v1',
 }
