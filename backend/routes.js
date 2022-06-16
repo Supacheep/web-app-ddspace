3ea7bf360b9b07e544efdb73994eab156199e238
supacheep <supacheep.1972@gmail.com>
2022-06-16 08:23:15 +0700

    clean

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,9 @@
-const authController = require('./controllers/authController')
+// const authController = require('./controllers/authController')
 
 const router = (app, client) => {
-  app.post('/auth/login', authController.login)
-  app.get('/auth/user', authController.getUserData)
-  app.delete('/auth/logout', authController.logout)
+  // app.post('/auth/login', authController.login)
+  // app.get('/auth/user', authController.getUserData)
+  // app.delete('/auth/logout', authController.logout)
 }
 
 module.exports = router
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import {
-  useContext,
+  // useContext,
   useState,
   useEffect,
 } from 'react'
 import {
   Layout,
-  Menu,
+  // Menu,
   Drawer,
-  Dropdown,
+  // Dropdown,
   Button,
 } from 'antd'
 import Link from 'next/link'
@@ -15,11 +15,11 @@ import '../styles/globals.css'
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 import Router, { useRouter } from 'next/router'
 import Head from 'next/head'
-import withUserContext from '../src/hoc/withUserContext'
-import userContext from '../src/context/userContext'
+// import withUserContext from '../src/hoc/withUserContext'
+// import userContext from '../src/context/userContext'
 import { colors } from '../src/configs/color'
 import { Burger, ProfileComponent } from '../src/components'
-import { HeaderLogo } from '../src/components/common'
+// import { HeaderLogo } from '../src/components/common'
 import { MOBILE_WINDOW_WIDTH } from '../src/constants/constantsValue'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 
@@ -69,23 +69,23 @@ const LinkButton = styled.a`
   ${(props) => (props.active ? `background-color: ${colors.greyF3}` : '')}
 `
 
-const LinkButtonMobile = styled.div`
-  padding: 10px;
-  border-radius: 8px;
-  cursor: pointer;
-  :hover {
-    background-color: ${colors.grey200};
-  }
-  ${(props) => (props.active ? `
-    /* background-color: ${colors.greyF3} */
-    background: white;
-    background: linear-gradient(
-      to right bottom,
-      rgba(255, 255, 255, 0.8),
-      rgba(255, 255, 255, 0.3)
-    );
-  ` : '')}
-`
+// const LinkButtonMobile = styled.div`
+//   padding: 10px;
+//   border-radius: 8px;
+//   cursor: pointer;
+//   :hover {
+//     background-color: ${colors.grey200};
+//   }
+//   ${(props) => (props.active ? `
+//     /* background-color: ${colors.greyF3} */
+//     background: white;
+//     background: linear-gradient(
+//       to right bottom,
+//       rgba(255, 255, 255, 0.8),
+//       rgba(255, 255, 255, 0.3)
+//     );
+//   ` : '')}
+// `
 
 const MenuContainer = styled.div`
   width: ${(props) => (props.justify === 'center' ? '50%' : '25%')};
@@ -136,19 +136,19 @@ const CustomDrawer = styled(Drawer)`
   }
 `
 
-const ProfileButton = styled(Button)`
-  height: auto;
-  padding: 0;
-  border: 0;
-  box-shadow: none;
-`
+// const ProfileButton = styled(Button)`
+//   height: auto;
+//   padding: 0;
+//   border: 0;
+//   box-shadow: none;
+// `
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   const { asPath, pathname } = useRouter()
   const [drawerVisible, setDrawerVisible] = useState(false)
   const [isMobile, setIsMobile] = useState(false)
-  const user = useContext(userContext)
+  // const user = useContext(userContext)
 
   const onResize = () => {
     const nextIsMobile = window.innerWidth <= MOBILE_WINDOW_WIDTH
@@ -157,15 +157,15 @@ function MyApp({ Component, pageProps }) {
     }
   }
 
-  useEffect(() => {
-    if (!hideHeaderList.includes(pathname)) {
-      user.fetchUser().then(({ userData }) => {
-        if (!userData) {
-          Router.push('/')
-        }
-      })
-    }
-  }, [asPath])
+  // useEffect(() => {
+  //   if (!hideHeaderList.includes(pathname)) {
+  //     user.fetchUser().then(({ userData }) => {
+  //       if (!userData) {
+  //         Router.push('/')
+  //       }
+  //     })
+  //   }
+  // }, [asPath])
 
   useEffect(() => {
     document.body.scrollTo(0, 0)
@@ -181,53 +181,22 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     onResize()
-    document.getElementById('holderStyle')?.remove()
   }, [])
-
-  const dropdownMenu = (
-    <Menu>
-      <Menu.Item onClick={() => user.logout()}>
-        <span>
-          Logout
-        </span>
-      </Menu.Item>
-    </Menu>
-  )
 
   const DesktopHeader = () => (
     <CustomHeader id="desktop-header">
       <Link href={{ pathname: '/' }}>
         <MenuContainer>
-          <HeaderLogo
-            src="/images/LOGOICS_.png"
-            alt="LogoICS"
-            style={{ marginTop: 16 }}
-          />
-          <HeaderLogo
-            src="/images/LogoAPALMS-01.svg"
-            alt="LogoThAPALMS"
-          />
+          <div>logo</div>
         </MenuContainer>
       </Link>
       <MenuContainer justify="center">
         <div>
-          <Link href={{ pathname: '/' }}>
-            <LinkButton active={asPath === '/'} href="/">Lobby</LinkButton>
-          </Link>
-          <Link href={{ pathname: '/conference' }}>
-            <LinkButton active={asPath === '/conference'} href="/conference">Conference Hall</LinkButton>
-          </Link>
-          <Link href={{ pathname: '/exhibition' }}>
-            <LinkButton active={asPath === '/exhibition'} href="/exhibition">Exhibition Hall</LinkButton>
-          </Link>
+          center
         </div>
       </MenuContainer>
       <MenuContainer justify="flex-end">
-        <Dropdown overlay={dropdownMenu} trigger={['click']}>
-          <ProfileButton>
-            <ProfileComponent name={user?.userData?.name} />
-          </ProfileButton>
-        </Dropdown>
+        <div>right</div>
       </MenuContainer>
     </CustomHeader>
   )
@@ -239,15 +208,7 @@ function MyApp({ Component, pageProps }) {
       </MenuContainer>
       <Link href={{ pathname: '/' }}>
         <MenuContainer justify="center">
-          <HeaderLogo
-            src="/images/LOGOICS_.png"
-            alt="LogoICS"
-            style={{ marginTop: 16 }}
-          />
-          <HeaderLogo
-            src="/images/LogoAPALMS-01.svg"
-            alt="LogoThAPALMS"
-          />
+          <div>logo</div>
         </MenuContainer>
       </Link>
       <MenuContainer justify="flex-end" />
@@ -257,7 +218,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <CustomLayout>
       <Head>
-        <title>ICSMeeting2021</title>
+        <title>SCG</title>
       </Head>
       {
         !hideHeaderList.includes(pathname) && (
@@ -275,23 +236,10 @@ function MyApp({ Component, pageProps }) {
         visible={drawerVisible}
         maskStyle={{ opacity: 0, animation: 'none' }}
       >
-        <Dropdown overlay={dropdownMenu} trigger={['click']}>
-          <ProfileButton>
-            <ProfileComponent name={user?.userData?.name} />
-          </ProfileButton>
-        </Dropdown>
-        <Link href={{ pathname: '/' }}>
-          <LinkButtonMobile active={asPath === '/'}>Lobby</LinkButtonMobile>
-        </Link>
-        <Link href={{ pathname: '/conference' }}>
-          <LinkButtonMobile active={asPath === '/conference'}>Conference Hall</LinkButtonMobile>
-        </Link>
-        <Link href={{ pathname: '/exhibition' }}>
-          <LinkButtonMobile active={asPath === '/exhibition'}>Exhibition Hall</LinkButtonMobile>
-        </Link>
+        <div>link</div>
       </CustomDrawer>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} isMobile={isMobile} userData={user} />
+        <Component {...pageProps} isMobile={isMobile} />
       </ThemeProvider>
       <GlobalStyle />
     </CustomLayout>
@@ -299,10 +247,10 @@ function MyApp({ Component, pageProps }) {
 }
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
-  if (ctx?.req?.cookies && !ctx.req.cookies.userData && !exceptLoginPath.includes(ctx.asPath)) {
-    ctx.res.redirect('/')
-    return {}
-  }
+  // if (ctx?.req?.cookies && !ctx.req.cookies.userData && !exceptLoginPath.includes(ctx.asPath)) {
+  //   ctx.res.redirect('/')
+  //   return {}
+  // }
 
   let pageProps = {}
 
@@ -313,4 +261,4 @@ MyApp.getInitialProps = async ({ Component, ctx }) => {
   return { pageProps }
 }
 
-export default withUserContext(MyApp)
+export default MyApp
